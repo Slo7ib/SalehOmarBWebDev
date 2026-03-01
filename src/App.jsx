0ede23ada0cbee4c91fd8ee652a77b3417fe3927
Saleh Blhmr <83185094+Slo7ib@users.noreply.github.com>
2026-03-01 20:29:39 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Experience } from "@/sections/Experience";
 import { Testimonials } from "@/sections/Testimonials";
 import { Contact } from "@/sections/Contact";
 import { Footer } from "./layout/Footer";
-// jsx
+
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
